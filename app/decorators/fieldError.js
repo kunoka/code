14653b0d21a065feadadbe6363bd6f2fb0b64a27
kunoka <57294450@qq.com>
2016-06-18 06:45:16 +0800

    2016-6-18 from E430

--- app/decorators/fieldError.js
+++ app/decorators/fieldError.js
@@ -6,11 +6,7 @@ angular.module("com.ngnice.app").directive('bfFieldError', function bfFieldError
         restrict: 'A',
         require: 'ngModel',
         link: function(scope, element, attrs, ngModel){
-            console.log(new Date());
-            console.log(ngModel);
             var subScope = scope.$new(true);
-            console.log(scope);
-            console.log(subScope);
             subScope.hasError = function(){
                 return ngModel.$invalid && ngModel.$dirty;
             };
